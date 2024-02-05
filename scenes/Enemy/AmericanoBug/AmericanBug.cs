5a7ab85adb31e52fff783081b9470c1e26fee813
Maro-3D <hanzelkamarek@protonmail.com>
2024-02-05 19:12:21 +0100

    LightFlicker

--- scenes/Enemy/AmericanoBug/AmericanBug.cs
+++ scenes/Enemy/AmericanoBug/AmericanBug.cs
@@ -13,136 +13,136 @@ namespace FiveNightsAtFrederik.Scenes.Enemy;
 /// </summary>
 public partial class AmericanBug : BaseEnemy, IMovableCharacter, IDamagable
 {
-    public float MovementSpeed { get; set; } = 400f;
-    public float JumpVelocity { get; set; }
-    public float RotationSpeed { get; set; } = 0.8f;
+	public float MovementSpeed { get; set; } = 400f;
+	public float JumpVelocity { get; set; }
+	public float RotationSpeed { get; set; } = 0.8f;
 
-    private readonly Random random = new(2);
-    private Timer activationTimer;
+	private readonly Random random = new(2);
+	private Timer activationTimer;
 
-    [ExportGroup("Dictionary<EnemySounds, AudioStreamMp3> EnumValues: 0:Deactivate, 1:Activate, 2:Jumpscare")]
-    [Export()]
-    private Dictionary<EnemySounds, AudioStreamMP3> audioTracks = new();
-    [ExportGroup("")]
+	[ExportGroup("Dictionary<EnemySounds, AudioStreamMp3> EnumValues: 0:Deactivate, 1:Activate, 2:Jumpscare")]
+	[Export()]
+	private Dictionary<EnemySounds, AudioStreamMP3> audioTracks = new();
+	[ExportGroup("")]
 
-    /// <summary>
-    /// Slowly interpolates between ForwardPosition and nextPosition, which allows for smoot rotation 
-    /// </summary>
-    private Vector3 interpolationCurrentPosition;
-    private Vector3 nextPosition;
-    private Vector3 spawnLocation;
-    private bool isHit;
+	/// <summary>
+	/// Slowly interpolates between ForwardPosition and nextPosition, which allows for smoot rotation 
+	/// </summary>
+	private Vector3 interpolationCurrentPosition;
+	private Vector3 nextPosition;
+	private Vector3 spawnLocation;
+	private bool isHit;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        activationTimer = GetNode<Timer>(NodeNames.BugActivationTimer.ToString()) ?? throw new NativeMemberNotFoundException($"Node: {Name} failed to find {nameof(activationTimer)} at {NodeNames.BugActivationTimer}");
-        activationTimer.Timeout += () => isActive = true;
+	public override void _Ready()
+	{
+		base._Ready();
+		activationTimer = GetNode<Timer>(NodeNames.BugActivationTimer.ToString()) ?? throw new NativeMemberNotFoundException($"Node: {Name} failed to find {nameof(activationTimer)} at {NodeNames.BugActivationTimer}");
+		activationTimer.Timeout += () => isActive = true;
 
-        interpolationCurrentPosition = LookForwardPosition.GlobalPosition;
-        spawnLocation = GlobalPosition;
+		interpolationCurrentPosition = LookForwardPosition.GlobalPosition;
+		spawnLocation = GlobalPosition;
 
-        animationTree.Active = true;
+		animationTree.Active = true;
 
-        // TODO: Set correct activation timer value
-        idleTimer.Start(5);
-    }
+		// TODO: Set correct activation timer value
+		idleTimer.Start(5);
+	}
 
-    /// <summary>
-    /// When target is reached, kill player.
-    /// If bug was hit, Deactivate him after getting back to spawn location
-    /// </summary>
-    protected override void OnTargetReached()
-    {
-        isActive = false;
+	/// <summary>
+	/// When target is reached, kill player.
+	/// If bug was hit, Deactivate him after getting back to spawn location
+	/// </summary>
+	protected override void OnTargetReached()
+	{
+		isActive = false;
 
-        if (isHit)
-        {
-            GD.Print("Bug Deactivated!");
-            idleTimer.Start(random.Next(30, 60));
-            isHit = false;
+		if (isHit)
+		{
+			GD.Print("Bug Deactivated!");
+			idleTimer.Start(random.Next(30, 60));
+			isHit = false;
 
-            return;
-        }
+			return;
+		}
 
-        currentAnimation = EnemyAnimationStates.Jumpscare;
-        audioPlayer.Stream = audioTracks[EnemySounds.Jumpscare];
-        audioPlayer.Play();
-        
-        player.HandleJumpscare(jumpscareCameraPositionMarker.GlobalPosition, GlobalPosition + new Vector3(0, 0.5f, 0));
-    }
+		currentAnimation = EnemyAnimationStates.Jumpscare;
+		audioPlayer.Stream = audioTracks[EnemySounds.Jumpscare];
+		audioPlayer.Play();
+		
+		player.HandleJumpscare(jumpscareCameraPositionMarker.GlobalPosition, GlobalPosition + new Vector3(0, 0.5f, 0));
+	}
 
-    /// <summary>
-    /// Tries to activate bug If succeded, there is a warmup, before it is unleashed
-    /// </summary>
-    protected override void OnIdleTimerTimeout()
-    {
-        var number = random.Next(11);
-        if (number > 3)
-        {
-            idleTimer.Start(random.Next(10, 20));
+	/// <summary>
+	/// Tries to activate bug If succeded, there is a warmup, before it is unleashed
+	/// </summary>
+	protected override void OnIdleTimerTimeout()
+	{
+		var number = random.Next(11);
+		if (number > 3)
+		{
+			idleTimer.Start(random.Next(10, 20));
 
-            GD.Print("Bug NOT Activated");
-            return;
-        }
+			GD.Print("Bug NOT Activated");
+			return;
+		}
 
-        audioPlayer.Stream = audioTracks[EnemySounds.Activate];
-        audioPlayer.Play();
+		audioPlayer.Stream = audioTracks[EnemySounds.Activate];
+		audioPlayer.Play();
 
-        idleTimer.Stop();
+		idleTimer.Stop();
 
-        if (activationTimer.TimeLeft == 0)
-        {
-            GD.Print("Bug Activated");
-            activationTimer.Start();
-        }
-    }
+		if (activationTimer.TimeLeft == 0)
+		{
+			GD.Print("Bug Activated");
+			activationTimer.Start();
+		}
+	}
 
-    protected override void Move(float delta)
-    {
-        if (!isHit)
-        {
-            navigationAgent.TargetPosition = player.GlobalPosition;
-        }
+	protected override void Move(float delta)
+	{
+		if (!isHit)
+		{
+			navigationAgent.TargetPosition = player.GlobalPosition;
+		}
 
-        nextPosition = navigationAgent.GetNextPathPosition();
+		nextPosition = navigationAgent.GetNextPathPosition();
 
-        Velocity = (nextPosition - GlobalPosition).Normalized() * delta * MovementSpeed;
+		Velocity = (nextPosition - GlobalPosition).Normalized() * delta * MovementSpeed;
 
-        MoveAndSlide();
-    }
+		MoveAndSlide();
+	}
 
-    protected override void Rotate(float delta)
-    {
-        interpolationCurrentPosition = nextPosition.Lerp(interpolationCurrentPosition, delta * RotationSpeed);
-        interpolationCurrentPosition.Y = GlobalPosition.Y;
+	protected override void Rotate(float delta)
+	{
+		interpolationCurrentPosition = nextPosition.Lerp(interpolationCurrentPosition, delta * RotationSpeed);
+		interpolationCurrentPosition.Y = GlobalPosition.Y;
 
-        LookAt(interpolationCurrentPosition);
-    }
+		LookAt(interpolationCurrentPosition);
+	}
 
-    protected override void HandleAnimations()
-    {
-        animationTree.Set(currentAnimation.GetDescription(), false);
+	protected override void HandleAnimations()
+	{
+		animationTree.Set(currentAnimation.GetDescription(), false);
 
-        if (currentAnimation != EnemyAnimationStates.Jumpscare)
-        {
-            currentAnimation = !Velocity.IsZeroApprox() ? EnemyAnimationStates.WalkForward : EnemyAnimationStates.Idle;
-        }
+		if (currentAnimation != EnemyAnimationStates.Jumpscare)
+		{
+			currentAnimation = !Velocity.IsZeroApprox() ? EnemyAnimationStates.WalkForward : EnemyAnimationStates.Idle;
+		}
 
-        animationTree.Set(currentAnimation.GetDescription(), true);
-    }
+		animationTree.Set(currentAnimation.GetDescription(), true);
+	}
 
 
-    /// <summary>
-    /// When bug is shot, set targetPostion = spawnLocation
-    /// </summary>
-    public void ApplyDamage()
-    {
-        if (isActive)
-        {
-            navigationAgent.TargetPosition = spawnLocation;
-            audioPlayer.Stop();
-            isHit = true;
-        }
-    }
+	/// <summary>
+	/// When bug is shot, set targetPostion = spawnLocation
+	/// </summary>
+	public void ApplyDamage()
+	{
+		if (isActive)
+		{
+			navigationAgent.TargetPosition = spawnLocation;
+			audioPlayer.Stop();
+			isHit = true;
+		}
+	}
 }
