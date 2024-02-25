0815997bc30af019300a221a9a9952ef974449d6
Jendx <62170918+Jendx@users.noreply.github.com>
2024-02-25 10:09:05 +0100

    Order minigame (#34)
    * Fix pizza out of bounds
    
    * Implementation completed
    
    * Added max distance difference
    
    * Reoder

---
+++ CsScripts/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using FiveNightsAtFrederik.CsScripts.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace FiveNightsAtFrederik.CsScripts.Controllers;
+
+public static class OrderController
+{
+    private static readonly Random random = new();
+    private static readonly List<PizzaType> handledOrders = new();
+
+    public static PizzaType CurrentOrder { get; private set; }
+
+    public static Action OnOrderCreated { get; internal set; }
+
+    public static void CreateOrder()
+    {
+        CurrentOrder = (PizzaType)random.Next(0, Enum.GetValues<PizzaType>().Length);
+        OnOrderCreated();
+    }
+}
---
+++ scenes/level/Minigames/TakeOrder/TakeOrderMinigame.cs
@@ -0,0 +1,57 @@
+using FiveNightsAtFrederik.CsScripts.Constants;
+using FiveNightsAtFrederik.CsScripts.Controllers;
+using FiveNightsAtFrederik.CsScripts.Enums;
+using FiveNightsAtFrederik.CsScripts.Extensions;
+using FiveNightsAtFrederik.CsScripts.Interfaces;
+using Godot;
+using Godot.Collections;
+using System;
+
+namespace FiveNightsAtFrederik.scenes.level.Minigames.TakeOrder;
+
+public partial class TakeOrderMinigame : Node3D, IPlayerUsable
+{
+    public bool IsInteractionUIDisplayed { get; set; }
+
+    [Export]
+    private AudioStreamMP3 Ringtone;
+
+    [Export]
+    private Dictionary<PizzaType, AudioStreamMP3> orderRecordings = new();
+   
+    private readonly Random random = new();
+    private AudioStreamPlayer3D audioPlayer;
+    private bool isRinging;
+
+    public override void _Ready()
+    {
+        audioPlayer = this.TryGetNode<AudioStreamPlayer3D>(NodeNames.AudioPlayer, nameof(audioPlayer));
+        OrderController.OnOrderCreated += StartRinging;
+
+        var timer = GetTree().CreateTimer(random.Next(3, 8));
+        timer.Timeout += () => OrderController.CreateOrder();
+    }
+
+    public void StartRinging()
+    {
+        audioPlayer.MaxDistance = 40;
+        audioPlayer.PlayStream(Ringtone);
+        isRinging = true;
+        IsInteractionUIDisplayed = true;
+    }
+
+    public void OnBeginUse()
+    {
+        if (!isRinging)
+        {
+            return;
+        }
+
+        isRinging = false;
+        IsInteractionUIDisplayed = false;
+        audioPlayer.MaxDistance = 20;
+        audioPlayer.PlayStream(orderRecordings[OrderController.CurrentOrder]);
+    }
+
+    public void OnEndUse() {}
+}
